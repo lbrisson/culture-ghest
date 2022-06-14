2935cc8ac6d305deb51b311c173ef3428ee55c59
Lesly <brissonlesly@gmail.com>
2022-06-14 12:26:53 -0400

    minor commit

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,22 @@
+import Navigator from "./Navigator.jsx";
+import Paperbase from  "./paperbase/Paperbase.jsx"
+
+
+export default function LandingPage() {
+
+    // var SpotifyWebApi = require('spotify-web-api-node');
+
+    // // credentials are optional
+    // var spotifyApi = new SpotifyWebApi({
+    //   clientId: 'fcecfc72172e4cd267473117a17cbd4d',
+    //   clientSecret: 'a6338157c9bb5ac9c71924cb2940e1a7',
+    //   redirectUri: 'http://www.example.com/callback'
+    // });
+
+    return (
+        <div>
+        <Navigator />
+        <Paperbase />
+        </div>
+    );
+}
