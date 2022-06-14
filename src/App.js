2f570593e2cb81a2f5bbc6713f0e87112902f143
Lesly <brissonlesly@gmail.com>
2022-06-14 09:37:00 -0400

    committing several component templates

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Button from '@mui/material/Button';
+import Footer from './components/footer/Footer';
+import HeroBanner from './components/heroBanner/HeroBanner';
+import SearchNavBar from './components/SearchNavBar.jsx/SearchNavBar';
+import MediaControlCard from './components/mediaCards/MediaControlCard';
+import DescriptionAlerts from './components/alerts/DescriptionAlerts';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <SearchNavBar />
+        {/* <HeroBanner /> */}
+        {/* <MediaControlCard /> */}
+      {/* <DescriptionAlerts /> */}
+      {/* <Footer /> */}
     </div>
   );
 }
